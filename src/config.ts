493b95005abb81c85ccad5726d01afaf1c7db974
Odécio Machado <odeciojunior@gmail.com>
2025-09-01 02:36:46 -0300

    posting...

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://blog.odeciomachado.com/", // replace this with your deployed domain
+  author: "Odécio Machado",
+  profile: "https://odecio.link/",
+  desc: "Um blog sobre tecnologia, programação e desenvolvimento pessoal.",
+  title: "Blog: Desce o Machado",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -18,6 +18,6 @@ export const SITE = {
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "pt", // html lang code. Set this empty and default will be "en"
+  timezone: "America/Sao_Paulo", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
