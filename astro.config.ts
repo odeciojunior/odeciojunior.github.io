b5f2ef100229ac2403b66337c0de607f76ece144
Odécio Machado <odeciojunior@gmail.com>
2025-09-01 04:00:21 -0300

    posting...

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: "https://odeciojunior.github.io",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
